21246beaf5815569fa269740563d17a812772ffd
Sergei Chaikosvky <s.v.chaikovsky@gmail.com>
2021-10-17 22:22:39 +0300

    fixed rundom number and changeHP

--- main.js
+++ main.js
@@ -1,17 +1,10 @@
 const $arenas = document.querySelector(".arenas");
-
 const $randomButton = document.querySelector(".button");
 
 const player1 = {
   player: 1,
   name: "Scorpion ",
   hp: 100,
-
-
-const player1 = {
-  name: "Scorpion ",
-  hp: 50,
-
   img: "http://reactmarathon-api.herokuapp.com/assets/scorpion.gif",
   weapon: [],
   attack() {
@@ -19,22 +12,16 @@ const player1 = {
   },
 };
 const player2 = {
-
   player: 2,
 
   name: "SUB-ZERO ",
   hp: 100,
-
-  name: "SUB-ZERO ",
-  hp: 80,
-
   img: "http://reactmarathon-api.herokuapp.com/assets/subzero.gif",
   weapon: [],
   attack() {
     console.log(player1.name + "Fight");
   },
 };
-
 
 function createDomElement(element, className) {
   const $element = document.createElement(element);
@@ -97,34 +84,3 @@ $randomButton.addEventListener("click", function () {
 
 $arenas.appendChild(createPlayer(player1));
 $arenas.appendChild(createPlayer(player2));
-
-function createPlayer(name, object) {
-  const $divPlayer1 = document.createElement("div");
-  $divPlayer1.classList.add(name);
-  $arenas.appendChild($divPlayer1);
-
-  const $divProgressbar = document.createElement("div");
-  $divProgressbar.classList.add("progressbar");
-  $divPlayer1.appendChild($divProgressbar);
-
-  const $divLife = document.createElement("div");
-  $divLife.classList.add("life");
-  $divLife.style.width = `${object.hp}%`;
-  $divProgressbar.appendChild($divLife);
-
-  const $divName = document.createElement("div");
-  $divName.classList.add("name");
-  $divName.innerHTML = object.name;
-  $divProgressbar.appendChild($divName);
-
-  const $divCharacter = document.createElement("div");
-  $divCharacter.classList.add("character");
-  $divPlayer1.appendChild($divCharacter);
-
-  const $img = document.createElement("img");
-  $img.src = object.img;
-  $divCharacter.appendChild($img);
-}
-createPlayer("player1", player1);
-createPlayer("player2", player2);
-
